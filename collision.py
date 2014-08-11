3c39218fcc6667701576ca005612bdbfa09575e6
jordan4ibanez <jordan4ibanez@gmail.com>
2014-08-11 16:28:08 -0400

    Start preparing code for inertia, inertia transmission, detection of all entities against all entities, and add code for color of entity

--- collision.py
+++ collision.py
@@ -13,32 +13,44 @@ entity_table = [
 	[ #entity 0
 		"rectangle",#shape
 		"entity", #physical or fixture
+		(255,0,0),
 		[
 
 			20,#pos  x
 			20,#pos  y
 			40,#size x
 			40,#size y
+			0,#inertia
 		],
 	],
 	[ #entity 1
 		"rectangle",#shape
 		"entity", #physical or fixture
+		(0,255,0),
 		[
 
 			200,#pos  x
 			200,#pos  y
-			90,#size x
-			90,#size y
+			20,#size x
+			20,#size y
+			0,#inertia
+		],
+	],
+	[ #entity 2
+		"rectangle",#shape
+		"entity", #physical or fixture
+		(0,0,255),
+		[
+
+			400,#pos  x
+			400,#pos  y
+			20,#size x
+			20,#size y
+			0,#inertia
 		],
 	],
 ]
-"""
-[0,0], #top left
-[40,0], #top right
-[40,40], #bottom right
-[0,40], #bottom left
-"""
+
 w = 0
 while w < 2000:
 	for event in pygame.event.get():
@@ -48,29 +60,30 @@ while w < 2000:
 	for x in range(len(entity_table)):
 		if entity_table[x][0] == "rectangle":
 			#take raw list data and convert to applicable rendering data
-			posx  = entity_table[x][2][0]
-			posy  = entity_table[x][2][1]
-			sizex = entity_table[x][2][2]
-			sizey = entity_table[x][2][3]
+			posx  = entity_table[x][3][0]
+			posy  = entity_table[x][3][1]
+			sizex = entity_table[x][3][2]
+			sizey = entity_table[x][3][3]
+			color = entity_table[x][2]
 			point1 = (posx - (sizex/2),posy - (sizey/2))
 			point2 = (posx + (sizex/2),posy - (sizey/2))
 			point3 = (posx + (sizex/2),posy + (sizey/2))
 			point4 = (posx - (sizex/2),posy + (sizey/2))
-			if x == 0:
-				pygame.draw.polygon(screen,black,(point1,point2,point3,point4))
-			if x == 1:
-				pygame.draw.polygon(screen,red,(point1,point2,point3,point4))
+			
+			pygame.draw.polygon(screen,color,(point1,point2,point3,point4))
 
 	pygame.display.flip()
 	
 	#Collision detections
 	#for i in range(len(entity_table)):
 	
+	entity_table[0][3][0],entity_table[0][3][1] = pygame.mouse.get_pos()
+	
 	#box 0 (black)
-	posax  = entity_table[0][2][0]
-	posay  = entity_table[0][2][1]
-	sizeax = entity_table[0][2][2]
-	sizeay = entity_table[0][2][3]
+	posax  = entity_table[0][3][0]
+	posay  = entity_table[0][3][1]
+	sizeax = entity_table[0][3][2]
+	sizeay = entity_table[0][3][3]
 	pointa1 = (posax - (sizeax/2),posay - (sizeay/2))#topleft
 	pointa2 = (posax + (sizeax/2),posay - (sizeay/2))#topright
 	pointa3 = (posax + (sizeax/2),posay + (sizeay/2))#bottomright
@@ -82,10 +95,10 @@ while w < 2000:
 	
 
 	#box1(red)
-	posbx  = entity_table[1][2][0]
-	posby  = entity_table[1][2][1]
-	sizebx = entity_table[1][2][2]
-	sizeby = entity_table[1][2][3]
+	posbx  = entity_table[1][3][0]
+	posby  = entity_table[1][3][1]
+	sizebx = entity_table[1][3][2]
+	sizeby = entity_table[1][3][3]
 	pointb1 = (posbx - (sizebx/2),posby - (sizeby/2))
 	pointb2 = (posbx + (sizebx/2),posby - (sizeby/2))
 	pointb3 = (posbx + (sizebx/2),posby + (sizeby/2))
@@ -104,12 +117,12 @@ while w < 2000:
 		
 		if xdiff >= ydiff:
 			if posax >= posbx:
-				entity_table[0][2][0] = posbx + ((sizeax/2)+(sizebx/2))
+				entity_table[0][3][0] = posbx + ((sizeax/2)+(sizebx/2))
 			if posax < posbx:
-				entity_table[0][2][0] = posbx - ((sizeax/2)+(sizebx/2))	
-		if xdiff <= ydiff:	
+				entity_table[0][3][0] = posbx - ((sizeax/2)+(sizebx/2))	
+		if xdiff < ydiff:	
 			if posay >= posby:
-				entity_table[0][2][1] = posby + ((sizeay/2)+(sizeby/2))
+				entity_table[0][3][1] = posby + ((sizeay/2)+(sizeby/2))
 			if posay < posby:
-				entity_table[0][2][1] = posby - ((sizeay/2)+(sizeby/2))	
+				entity_table[0][3][1] = posby - ((sizeay/2)+(sizeby/2))	
 		
