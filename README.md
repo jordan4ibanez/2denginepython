c150b181a99e3d6f451703e721d507241a4f5790
jordan4ibanez <jordan4ibanez@gmail.com>
2014-08-17 03:08:46 -0400

    do more advanced physics using inertia, and basic inertia transfers through entities with infinite efficiency for now

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ todo
 perfect collision detection
 
 Make collision detection have a predicted position to move entities to, then take average of all entities and move
+
+bounciness for entities
+
+transfer momentum based on weight and bounciness (almost done -bounciness)
+
+include bounciness in coll detec
+
+calculate collision detection based on size, inertia, and position instead of just inertia and position
+
+eventually include friction 
--- collision.py
+++ collision.py
@@ -3,6 +3,7 @@ import pygame,sys,os,time
 screensize = width,height = 1000,800
 screen = pygame.display.set_mode(screensize)
 pygame.init()
+font = pygame.font.Font("font/Kirvy-Regular.otf", 10)
 
 black = 0,0,0
 white = 255,255,255
@@ -16,11 +17,12 @@ entity_table = [
 		(255,0,0),
 		[
 
-			20,#pos  x
-			20,#pos  y
+			50,#pos  x
+			400,#pos  y
 			40,#size x
 			40,#size y
-			0,#inertia
+			[0,0],#inertia
+			100,#bounce %
 		],
 	],
 	[ #entity 1
@@ -29,11 +31,12 @@ entity_table = [
 		(0,255,0),
 		[
 
-			200,#pos  x
-			200,#pos  y
+			700,#pos  x
+			400,#pos  y
 			60,#size x
 			60,#size y
-			0,#inertia
+			[0,0],#inertia
+			100,#bounce %
 		],
 	],
 	[ #entity 2
@@ -46,7 +49,21 @@ entity_table = [
 			400,#pos  y
 			30,#size x
 			30,#size y
-			0,#inertia
+			[0,0],#inertia
+			100,#bounce %
+		],
+	],
+	[ #entity 3
+		"rectangle",#shape
+		"entity", #physical or fixture
+		(0,0,255),
+		[
+			500,#pos  x
+			400,#pos  y
+			30,#size x
+			30,#size y
+			[0,0],#inertia
+			100,#bounce %
 		],
 	],
 ]
@@ -71,18 +88,56 @@ while w < 2000:
 			point4 = (posx - (sizex/2),posy + (sizey/2))
 			
 			pygame.draw.polygon(screen,color,(point1,point2,point3,point4))
-
+			text = font.render(str(x), 1, (10, 10, 10))
+			screen.blit(text, (posx,posy))
+			
 	pygame.display.flip()
 	
+	
+	#debug for player entity
+	#clean this up and make this it's own function eventually
+	
+	key = pygame.key.get_pressed()
+	#X
+	if key[pygame.K_RIGHT]:
+		entity_table[1][3][4][0] = entity_table[1][3][4][0] + 0.1
+	elif key[pygame.K_LEFT]:
+		entity_table[1][3][4][0] = entity_table[1][3][4][0] - 0.1
+	#slowdown x
+	else:
+		if entity_table[1][3][4][0] > 0:
+			entity_table[1][3][4][0] = entity_table[1][3][4][0] - 0.1
+		elif entity_table[1][3][4][0] < 0:
+			entity_table[1][3][4][0] = entity_table[1][3][4][0] + 0.1
+	#correct for floating point error
+	if entity_table[1][3][4][0] < 0.1 and entity_table[1][3][4][0] > -0.1:
+		entity_table[1][3][4][0] = 0
+	#Y	
+	if key[pygame.K_UP]:
+		entity_table[1][3][4][1] = entity_table[1][3][4][1] - 0.1
+	elif key[pygame.K_DOWN]:
+		entity_table[1][3][4][1] = entity_table[1][3][4][1] + 0.1
+	#slowdown Y
+	else:
+		if entity_table[1][3][4][1] > 0:
+			entity_table[1][3][4][1] = entity_table[1][3][4][1] - 0.1
+		elif entity_table[1][3][4][1] < 0:
+			entity_table[1][3][4][1] = entity_table[1][3][4][1] + 0.1
+	#correct for floating point error
+	if entity_table[1][3][4][1] < 0.1 and entity_table[1][3][4][1] > -0.1:
+		entity_table[1][3][4][1] = 0
+	
+	#momentum	
+	for x in range(len(entity_table)):
+		entity_table[x][3][0] = entity_table[x][3][0] + entity_table[x][3][4][0]
+		entity_table[x][3][1] = entity_table[x][3][1] + entity_table[x][3][4][1]
+	
 	#Collision detections
-	#for i in range(len(entity_table)):
-	
-	entity_table[0][3][0],entity_table[0][3][1] = pygame.mouse.get_pos()
-	
 	for x in range(len(entity_table)):
 		for y in range(len(entity_table)):
 			if x == y:
 				break
+				
 			#box 0 (black)
 			posax  = entity_table[x][3][0]
 			posay  = entity_table[x][3][1]
@@ -96,8 +151,8 @@ while w < 2000:
 			bottoma = (pointa3[1]+pointa4[1])/2
 			lefta   = (pointa1[0]+pointa4[0])/2
 			righta  = (pointa2[0]+pointa3[0])/2
-			
-
+			inertxa = entity_table[x][3][4][0]
+			inertya = entity_table[x][3][4][1] 
 			#box1(red)
 			posbx  = entity_table[y][3][0]
 			posby  = entity_table[y][3][1]
@@ -110,23 +165,68 @@ while w < 2000:
 			topb    = (pointb1[1]+pointb2[1])/2
 			bottomb = (pointb3[1]+pointb4[1])/2
 			leftb   = (pointb1[0]+pointb4[0])/2
-			rightb  = (pointb2[0]+pointb3[0])/2	
+			rightb  = (pointb2[0]+pointb3[0])/2
+			inertxb = entity_table[y][3][4][0]
+			inertyb = entity_table[y][3][4][1]
 			
-
+			#if outside range pass, if in, do bounding box sorting
 			if topa > bottomb or bottoma < topb or lefta > rightb or righta < leftb:
 				pass
 			else:
+
 				xdiff = abs(posax-posbx)
 				ydiff = abs(posay-posby)
-				
 				if xdiff >= ydiff:
-					if posax >= posbx:
-						entity_table[x][3][0] = posbx + ((sizeax/2)+(sizebx/2))
-					if posax < posbx:
-						entity_table[x][3][0] = posbx - ((sizeax/2)+(sizebx/2))	
-				if xdiff < ydiff:	
-					if posay >= posby:
-						entity_table[x][3][1] = posby + ((sizeay/2)+(sizeby/2))
-					if posay < posby:
-						entity_table[x][3][1] = posby - ((sizeay/2)+(sizeby/2))	
-				
+					#compare inertia of a to b
+					if abs(inertxa) < abs(inertxb):
+						if posax >= posbx:
+							entity_table[x][3][0] = posbx + ((sizeax/2)+(sizebx/2)) + 1
+							#transfer inertia
+							entity_table[x][3][4][0] = entity_table[y][3][4][0]
+						if posax < posbx:
+							entity_table[x][3][0] = posbx - ((sizeax/2)+(sizebx/2)) - 1
+							entity_table[x][3][4][0] = entity_table[y][3][4][0]
+					if abs(inertxa) > abs(inertxb):
+						if posax <= posbx:
+							entity_table[y][3][0] = posax + ((sizeax/2)+(sizebx/2)) + 1
+							entity_table[y][3][4][0] = entity_table[x][3][4][0]
+						if posax > posbx:
+							entity_table[y][3][0] = posax - ((sizeax/2)+(sizebx/2)) - 1
+							entity_table[y][3][4][0] = entity_table[x][3][4][0]
+					#do equal inertia for kicks
+					if abs(inertxa) == abs(inertxb):
+						if posax <= posbx:
+							entity_table[y][3][0] = posax + ((sizeax/2)+(sizebx/2)) + 1
+							entity_table[y][3][4][0] = 0
+							entity_table[x][3][4][0] = 0							
+						if posax > posbx:
+							entity_table[y][3][0] = posax - ((sizeax/2)+(sizebx/2)) - 1
+							entity_table[y][3][4][0] = 0
+							entity_table[x][3][4][0] = 0
+				if xdiff <= ydiff:
+					#compare inertia of a to b
+					if abs(inertya) < abs(inertyb):
+						if posay >= posby:
+							entity_table[x][3][1] = posby + ((sizeay/2)+(sizeby/2)) + 2
+							#transfer inertia
+							entity_table[x][3][4][1] = entity_table[y][3][4][1]
+						if posay < posby:
+							entity_table[x][3][1] = posby - ((sizeay/2)+(sizeby/2)) - 2
+							entity_table[x][3][4][1] = entity_table[y][3][4][1]
+					if abs(inertya) > abs(inertyb):
+						if posay <= posby:
+							entity_table[y][3][1] = posay + ((sizeay/2)+(sizeby/2)) + 2
+							entity_table[y][3][4][1] = entity_table[x][3][4][1]
+						if posay > posby:
+							entity_table[y][3][1] = posay - ((sizeay/2)+(sizeby/2)) - 2
+							entity_table[y][3][4][1] = entity_table[x][3][4][1]
+					#do equal inertia for kicks
+					if abs(inertya) == abs(inertyb):
+						if posay <= posby:
+							entity_table[y][3][1] = posay + ((sizeay/2)+(sizeby/2)) + 2
+							entity_table[y][3][4][1] = 0
+							entity_table[x][3][4][1] = 0							
+						if posay > posby:
+							entity_table[y][3][1] = posay - ((sizeay/2)+(sizeby/2)) - 2
+							entity_table[y][3][4][1] = 0
+							entity_table[x][3][4][1] = 0
